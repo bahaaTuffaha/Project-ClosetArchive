0718ded82131907a23c1aebe3597fc4532354792
bahaa <bahaa.tuffaha@yahoo.com>
2023-08-01 01:19:42 +0300

    Fixing and optimizing item filtering

--- screens/bottomNav/HomeBottom.tsx
+++ screens/bottomNav/HomeBottom.tsx
@@ -8,12 +8,43 @@ import { useSelector } from "react-redux";
 import { RootState } from "../../redux/store";
 import { ItemBox } from "../../components/ItemBox";
 import { CollectionContainer } from "../../components/CollectionContainer";
+import { item } from "../../redux/itemsSlice";
 export function HomeBottom() {
   //this is the main page
   const itemsState = useSelector((state: RootState) => state.itemsList);
   const [search, setSearch] = useState("");
   const [isSearchVisible, setIsSearchVisible] = useState(false);
   const isDarkMode = useColorScheme() === "dark";
+  const [allCategories, setAllCategories] = useState<item[][]>([]);
+  const [nonCategorized, setNonCategorized] = useState<item[]>([]);
+  useEffect(() => {
+    let cat = [];
+    let nonCat = [];
+    let final = [];
+    for (let i = 0; i < itemsState.collectionTags.length; i++) {
+      cat = [];
+      nonCat = [];
+      for (let j = 0; j < itemsState.items.length; j++) {
+        if (
+          itemsState.items[j].collection?.includes(
+            itemsState.collectionTags[i].value,
+          )
+        ) {
+          cat.push(itemsState.items[j]);
+        } else if (itemsState.items[j].collection?.length == 0) {
+          nonCat.push(itemsState.items[j]);
+        }
+      }
+      final.push(cat);
+    }
+    setAllCategories(final);
+    setNonCategorized(nonCat);
+    return () => {
+      cat = null;
+      nonCat = null;
+      final = null;
+    };
+  }, [itemsState.items.length]); // I will look for a solution in case of editing the items.
 
   return (
     <ThemeView classNameStyle="px-5">
@@ -62,15 +93,15 @@ export function HomeBottom() {
           )}
           <View className="w-full h-3/4 flex flex-row flex-wrap px-[5%] bg-gray mt-[1%] pt-2">
             {itemsState.collectionTags.map((collection, index) => {
-              return (
-                <CollectionContainer
-                  key={index}
-                  color={collection.color}
-                  label={collection.label}
-                >
-                  <>
-                    {itemsState.items.map((item) => {
-                      if (item.collection?.includes(collection.value)) {
+              if (allCategories[index]?.length ?? 0 != 0) {
+                return (
+                  <CollectionContainer
+                    key={index}
+                    color={collection.color}
+                    label={collection.label}
+                  >
+                    <>
+                      {allCategories[index].map((item) => {
                         return (
                           <ItemBox
                             primary={item.primaryColor || "#fff"}
@@ -82,25 +113,24 @@ export function HomeBottom() {
                             categoryNumber={item.category}
                           />
                         );
-                      }
-                    })}
-                  </>
-                </CollectionContainer>
-              );
-            })}
-            {itemsState.items.map((item) => {
-              if (item.collection?.length == 0)
-                return (
-                  <ItemBox
-                    primary={item.primaryColor || "#fff"}
-                    secondary={item.secondaryColor || "#fff"}
-                    tertiary={item.tertiaryColor || "#fff"}
-                    key={item.id}
-                    image={item.image}
-                    name={item.name}
-                    categoryNumber={item.category}
-                  />
+                      })}
+                    </>
+                  </CollectionContainer>
                 );
+              }
+            })}
+            {nonCategorized.map((item, index) => {
+              return (
+                <ItemBox
+                  primary={item.primaryColor || "#fff"}
+                  secondary={item.secondaryColor || "#fff"}
+                  tertiary={item.tertiaryColor || "#fff"}
+                  key={index + item.id}
+                  image={item.image}
+                  name={item.name}
+                  categoryNumber={item.category}
+                />
+              );
             })}
           </View>
         </View>
