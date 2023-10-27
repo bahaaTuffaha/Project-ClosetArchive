f7c3a11ffcdc2294d717198f5da66492314b08b5
bahaa <bahaa.tuffaha@yahoo.com>
2023-10-27 19:53:13 +0300

    Changing name again to "Closet-Archive"

--- ios/closetArchive/AppDelegate.mm
+++ ios/closetArchive/AppDelegate.mm
@@ -6,7 +6,7 @@
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions
 {
-  self.moduleName = @"closetArchive";
+  self.moduleName = @"Closet-Archive";
   // You can add your custom initial props in the dictionary below.
   // They will be passed down to the ViewController used by React Native.
   self.initialProps = @{};
